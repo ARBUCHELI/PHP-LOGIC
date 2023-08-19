35e9eb17b5713dd1ae3affb4c099ce7226eb23a6
ARBUCHELI <radio_seattle@hotmail.com>
2023-08-18 20:33:58 -1000

    first steps to build the lookAround function project save the farm

--- project-save-the-farm/lookAround.php
+++ project-save-the-farm/lookAround.php
@@ -2,16 +2,10 @@
   // Describe current location
 	function lookAround(){
 	// Write your code here:
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  global $location, $wearing_glasses, $wearing_contacts, $moved_cupboard;
+  if ($wearing_glasses xor $wearing_contacts) {
+
+  } else {
+      echo "It's really hard to make out any details...\n";
+  }
 }
